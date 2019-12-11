0ee7b99f23d5b0540b058a2d3b655709e5f8c87e
DorikSetyawan <dorik655@gmail.com>
2019-12-11 20:10:23 +0700

    PERTEMUAN12

---
+++ app/src/main/java/com/example/pert12/api/PhotosApi.kt
@@ -0,0 +1,12 @@
+package com.example.pert12.api
+
+
+import android.provider.ContactsContract
+import com.example.pert12.model.Photo
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface PhotosApi {
+    @GET("photos")
+    fun getPhotos(): Single<List<Photo>>
+}
